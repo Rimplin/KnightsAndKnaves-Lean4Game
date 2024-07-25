a21a729121e4859c8a3b909567ab453a189a5c76
Jad <jad-abou-hawili@hotmail.com>
2024-07-25 14:30:30 +0300

    Building Truth Table idea complete, figure out how the skeleton in editor mode should look like

--- Game/Levels/LogicTruthValue_/L01_And.lean
+++ Game/Levels/LogicTruthValue_/L01_And.lean
@@ -12,10 +12,11 @@ Introduction
 
 
 -- this is viable, the only issue is that the user has to explicitly go to the truth functional world, can this be forced somehow?? yes i can, by defining it and having the user user it. first make a level like this then make it easier for the user...
-variable {emTruth : P = True ∨ P = False}
+variable {emTruth : (P : Prop) → P = True ∨ P = False}
 Statement : P = (P ∧ P) := by 
   --apply @iff_eq_eq.mpr P P 
-  cases emTruth
+
+  cases emTruth P
   · 
     rw [h] 
     rw [and_true] 
